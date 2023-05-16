f01b8527c2cb2aece8d285d65dfc4d935ddb4891
nandstand <christ.fullr@gmail.com>
2023-05-16 00:54:47 -0500

    Finish up bare minimum implemntation of calendar

--- app.js
+++ app.js
@@ -1,3 +1,6 @@
+const serverUrl = 'http://localhost:3000';
+
+
 // Get the current date
 const currentDate = new Date();
 let currentMonth = currentDate.getMonth();
@@ -6,6 +9,7 @@ let currentYear = currentDate.getFullYear();
 // Generate the initial calendar for the current month and year
 generateCalendar(currentMonth, currentYear);
 
+// Button event listeners for the previous and next buttons
 document.getElementById('prevMonth').addEventListener('click', () => {
   currentMonth--;
   if (currentMonth < 0) {
@@ -24,15 +28,15 @@ document.getElementById('nextMonth').addEventListener('click', () => {
   generateCalendar(currentMonth, currentYear);
 });
 
-
+// Generate the calendar for the given month and year
 function generateCalendar(month, year) {
   // Create a new date object for the first day of the month
   const firstDay = new Date(year, month, 1);
 
   // Calculate the number of days in the month
-  const daysInMonth = new Date(year, month + 1, 0).getDate();
+  const daysInMonth = new Date(year, month + 1, 0).getDate(); // Next month's previous month's last day... 0 = last day of previous month
 
-  // Calculate the index of the first day (0 = Sunday, 1 = Monday, etc.)
+  // Calculate the index of the first day in the calendar grid
   const firstDayIndex = firstDay.getDay();
 
   // Generate the calendar structure based on the given month and year
@@ -43,6 +47,7 @@ function generateCalendar(month, year) {
     calendarHTML += `<div style="width: 14.28%; height: 100px;"></div>`;
   }
 
+  // Add a numbered square for each day of the month
   for (let day = 1; day <= daysInMonth; day++) {
     calendarHTML += `
         <div class="day border border-dark p-2" data-date="${year}-${month + 1}-${day}" style="width: 14.28%; height: 120px;">
@@ -60,7 +65,7 @@ function generateCalendar(month, year) {
 
   // Attach click event listeners to the days and load stored colors
   attachDayEventListeners();
-}
+} // End of generateCalendar function
 
 
 function getMonthName(month) {
@@ -71,9 +76,11 @@ function getMonthName(month) {
   return monthNames[month];
 }
 
+// Color picker
+
 const colorPicker = document.getElementById('colorPicker');
 const closeIcon = colorPicker.querySelector('.close-icon');
-const colors = ['#2ecc71', '#58d68d', '#7dcea0', '#a3e4d7', '#d6eaf8'];
+const colors = ['#2ecc71', '#58d68d', '#7dcea0', '#a3e4d7', '#d6eaf8', '#f5f5f5'];
 
 
 const colorCircles = colors.map(color => {
@@ -83,9 +90,23 @@ const colorCircles = colors.map(color => {
   circle.addEventListener('click', () => {
     colorPicker.classList.add('d-none');
     currentDay.style.backgroundColor = color;
-    // Store the selected color in localStorage
-    localStorage.setItem(`moodColor-${currentDay.dataset.date}`, color);
+
+    fetch(`${serverUrl}/mood/${currentDay.dataset.date}`, {
+      method: 'PUT',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({ color: color }),
+    })
+      .then(response => response.json())
+      .then(data => {
+        console.log('Success:', data);
+      })
+      .catch((error) => {
+        console.error('Error:', error);
+      });
   });
+
   return circle;
 });
 
@@ -105,49 +126,63 @@ function showColorPicker(event, dayElement) {
   if (colorPicker.style.display === 'block') {
     colorPicker.style.display = 'none';
 
-    // Use setTimeout to introduce a small delay (100ms) before showing the color picker again.
+    // Small 100ms delay before showing the color picker again
     setTimeout(() => {
       colorPicker.style.display = 'block';
-      positionColorPicker(event);
     }, 100);
   } else {
     colorPicker.style.display = 'block';
-    positionColorPicker(event);
   }
 }
 
 closeIcon.addEventListener('click', () => colorPicker.classList.add('d-none'));
 
-// Make sure to close the color picker when clicking outside of it
+//  close the color picker when clicking outside of it
 document.addEventListener('click', (event) => {
   if (!colorPicker.contains(event.target)) {
     colorPicker.classList.add('d-none');
   }
 });
 
-// Add click event to each day square
-document.querySelectorAll('.day').forEach(day => {
-  day.addEventListener('click', (event) => showColorPicker(event, day));
+// Attach event listeners to each day
+// This function is called once at startup
+// and after changing the month
 
-  // Load stored colors from localStorage
-  const storedColor = localStorage.getItem(`moodColor-${day.dataset.date}`);
-  if (storedColor) {
-    day.style.backgroundColor = storedColor;
-  }
-});
+// This function also loads the stored colors for each day
+// This function does a lot of things
+// Too many things
 
 function attachDayEventListeners() {
-  // Add click event to each day square
   document.querySelectorAll('.day').forEach(day => {
     day.addEventListener('click', (event) => showColorPicker(event, day));
 
-    // Load stored colors from localStorage
-    const storedColor = localStorage.getItem(`moodColor-${day.dataset.date}`);
-    if (storedColor) {
-      day.style.backgroundColor = storedColor;
-    }
+    fetch(`${serverUrl}/mood/${day.dataset.date}`)
+      .then(response => {
+        if (!response.ok) {
+          if (response.status === 404) {
+            // If the mood for this date is not found, set the color to the default
+            throw new Error('404');
+          } else {
+            throw new Error('Network response was not ok');
+          }
+        }
+        return response.json();
+      })
+      .then(mood => {
+        day.style.backgroundColor = mood.color;
+      })
+      .catch(error => {
+        if (error.message === '404') {
+          // Default color for dates with no mood set
+          day.style.backgroundColor = '#f5f5f5';
+        } else {
+          console.error('Error:', error);
+        }
+      });
+
   });
 }
+
 
 // Call the attachDayEventListeners function after generating the initial calendar
 attachDayEventListeners();
